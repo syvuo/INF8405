9d1c205cfd018f03ac83c4d17ced01b1fac44d5d
sudoyum21 <ba.samson.lam@gmail.com>
2017-01-26 20:02:42 -0500

    samson test commit - menu layout created

---
+++ app/src/main/java/com/example/sly/a8405_tp1/ui/GameGrid.java
@@ -0,0 +1,8 @@
+package com.example.sly.a8405_tp1.ui;
+
+/**
+ * Created by Lam, Vuong, Cloutier on 1/26/2017.
+ */
+
+public class GameGrid {
+}
--- app/src/main/java/com/example/sly/a8405_tp1/ui/MainActivity.java
+++ app/src/main/java/com/example/sly/a8405_tp1/ui/MainActivity.java
@@ -1,13 +1,14 @@
-package com.example.sly.a8405_tp1;
+package com.example.sly.a8405_tp1.ui;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.sly.a8405_tp1.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
+
+    public void startButtonClicked(View v){
+        popToast("Starting new game");
+    }
+
+    public void rulesButtonClicked(View v){
+        popToast("Rules Clicked");
+
+    }
+
+    public void exitButtonClicked(View v){
+        popToast("Application Closed");
+        finish();
+    }
+
+    private void popToast(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO DELETE
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
